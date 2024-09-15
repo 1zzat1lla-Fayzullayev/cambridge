c8bc1fab80f762e28f710c05ba75177097e59cdb
AbdulazeezwithWerr <abdulazizmamdaliyev16@gmail.com>
2024-09-15 13:56:26 +0500

    add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
+/* eslint-disable no-unused-vars */
+import { useEffect, useState } from "react"
+import Header from "./components/header"
 import Navbar from "./components/navbar"
 
 function App() {
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false)
+    }, 1000)
+
+    return () => clearTimeout(timer)
+  }, [])
+
+  if (loading) {
+    return (
+      <div className="flex flex-col items-center justify-center fixed left-0 top-0 h-full w-full z-[1000] bg-white overflow-hidden"><img src="/public/loading-logo.svg" className="infinite-scale w-[80px] h-[80px]" alt="Loading..." /></div>
+    )
+  }
   return (
     <>
+
       <Navbar />
+      <Header />
     </>
   )
 }
---
+++ src/components/header.jsx
@@ -0,0 +1,62 @@
+import Wrapper from "../layout/wrapper";
+import { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
+
+function Header() {
+    useEffect(() => {
+        AOS.init(); // Initialize AOS (Animate On Scroll) library
+    }, []);
+
+    return (
+        <Wrapper>
+            <section className="flex flex-col lg:flex-row justify-between items-center py-12 overflow-x-hidden">
+                <div
+                    data-aos="fade-right"
+                    data-aos-duration="500"
+                    className="md:w-[50%]"
+                >
+                    <h1 className="text-[#050a41] dark:text-white font-bold text-5xl lg:text-6xl xl:text-7xl text-start">
+                        Мы не просто обучаем английскому, мы меняем жизни людей!
+                    </h1>
+                    <p className="py-5 text-base lg:text-lg text-[#050a41] dark:text-white dark:opacity-50 text-start">
+                        За 9 лет Cambridge способствовал изменению жизни более 50 000 молодых людей
+                    </p>
+                    <a
+                        href="/ru/registration"
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="block w-full max-w-max "
+                    >
+                        <button
+                            aria-label="Register for Cambridge"
+                            className="flex items-center justify-center px-8 py-4 rounded-2xl duration-200 blue-gradient w-full max-w-max"
+                        >
+                            <span className="text-base lg:text-lg text-white">
+                                Запишитесь на первый урок
+                            </span>
+                            <img
+                                src="/stars.svg"
+
+                                className="ml-2"
+                            />
+                        </button>
+                    </a>
+                </div>
+                <div
+                    data-aos="fade-left"
+                    data-aos-duration="500"
+                    className="md:w-[50%] lg:w-[53%] mt-7 flex justify-center items-center"
+                >
+                    <img
+                        src="/books.png"
+                        alt="Books illustrating English learning"
+                        className="max-w-full h-auto"
+                    />
+                </div>
+            </section>
+        </Wrapper>
+    );
+}
+
+export default Header;
