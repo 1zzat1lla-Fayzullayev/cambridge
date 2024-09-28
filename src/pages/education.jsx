029c41bbfdac5d916b6ceabb65ab78c0dda9bbd1
AbdulazeezwithWerr <abdulazizmamdaliyev16@gmail.com>
2024-09-28 14:41:50 +0500

    change

--- src/pages/education.jsx
+++ src/pages/education.jsx
@@ -13,13 +13,22 @@ function Education() {
       style={{ minHeight: "calc(100vh - 22.8rem)" }}
     >
       <Wrapper>
-        <div className="space-y-8 px-4 md:px-6 lg:px-8">
-          <h2 className="text-2xl font-semibold text-mainBlue dark:text-white md:text-3xl xl:text-4xl">
-            {getText("educationTitle")}
-          </h2>
-          <p className="text-base text-gray-700 dark:text-gray-300 md:text-lg xl:text-xl leading-relaxed">
-            {getText("educationParagraph")}
-          </p>
+        <div className="flex flex-col md:flex-row items-center md:space-x-8">
+          <div className="space-y-8 px-4 md:px-6 lg:px-8 md:w-1/2">
+            <h2 className="text-3xl font-bold text-mainBlue dark:text-white md:text-4xl xl:text-5xl">
+              {getText("educationTitle")}
+            </h2>
+            <p className="text-lg text-gray-700 dark:text-gray-300 leading-relaxed">
+              {getText("educationParagraph")}
+            </p>
+          </div>
+          <div className="md:w-1/2">
+            <img
+              src="https://media.licdn.com/dms/image/C4E12AQH3Bxq-nsSaTg/article-cover_image-shrink_600_2000/0/1568650682842?e=2147483647&v=beta&t=J3Hv1EcmrUe2Q5wtxY2VlpM6Wwg9nBDjPvdzC0b4qC4"
+              alt="Education Illustration"
+              className="w-full h-auto rounded-lg transition-transform duration-300 transform hover:scale-105"
+            />
+          </div>
         </div>
       </Wrapper>
     </div>
